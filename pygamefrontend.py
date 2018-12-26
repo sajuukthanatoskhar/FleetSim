38eee0c2a5be9ba4888c30b58d0dfb86882c6b05
sajuukthanatoskhar <gareth_willia64@hotmail.com>
2018-12-26 23:45:36 +1100

    Initial Commit Phase 1

--- pygamefrontend.py
+++ pygamefrontend.py
@@ -2,4 +2,24 @@ import pygame
 
 pygame.init()
 
+white = (255,255,255)
+black = (0,0,0)
 
+red = (255,0,0)
+green = (0,255,0)
+blue = (0,0,255)
+
+gameDisplay = pygame.display.set_mode((800,600))
+gameDisplay.fill(black)
+pygame.draw.line(gameDisplay, blue, (100,200), (300,450),5)
+pixAr = pygame.PixelArray(gameDisplay)
+pixAr[10][20] = green
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            quit()
+
+
+    pygame.display.update()
--- ship.py
+++ ship.py
@@ -1,20 +1,74 @@
 #! /usr/bin/env python
+import numpy as np
 
-
+class location:
+    def __init__(self,x,y,z):
+        self.x = x
+        self.y = y
+        self.z = z
 
 class ship:
-    def __init__(self, hitpoints,damage,range,speed,inertia,name):
+    def __init__(self, hitpoints,damage,range,speed,inertia,name,x,y,z):
         self.hp = hitpoints
         self.dps = damage
         self.range = range
         self.speed = speed
         self.inertia = inertia
         self.name = name
+        self.loc = location(x,y,z)
+        if self.dps < 0:
+            self.is_logi = True
+        else:
+            self.is_logi = False
 
+
+    def check_range(self,target):
+        x = self.loc.x - target.loc.x
+        y = self.loc.y - target.loc.y
+        z = self.loc.z - target.loc.z
+
+        mag = np.array([x,y,z])
+        return np.linalg.norm(mag)
 
     def attack(self,target):
-        pass
+        if self.check_range(target) <= self.range:
+            target.hp -= self.dps
 
     def update_health(self):
         pass
+
+    def move_to(self,target):
+        x = self.loc.x - target.loc.x
+        y = self.loc.y - target.loc.y
+        z = self.loc.z - target.loc.z
+
+        mag = np.array([x,y,z])
+        mag = np.linalg.norm(mag)
+        x *= self.speed/mag
+        y *= self.speed/mag
+        z *= self.speed/mag
+        self.loc.x -= x
+        self.loc.y -= y
+        self.loc.z -= z
+        print(str(self.loc.x) + " " + str(self.loc.y) + " " + str(self.loc.z))
+        print("Range of ship : " + str(self.range))
+
+    def main_attack_procedure(self,target):
+        print("main_attack_procedure" + str(self.check_range(target)))
+        if self.check_range(target) > self.range:
+            self.move_to(target)
+        else:
+            self.attack(target)
+
+ship1 = ship(50,10,10,5,1,"Thanatos",50,100,20)
+ship2 = ship(50,4,50,2,1,"BoopityBoppity",50,150,20)
+
+
+while ship2.hp > 0:
+    ship1.main_attack_procedure(ship2)
+    #ship1.attack(ship2)
+    print(ship2.hp)
+    if(ship2.hp <= 0):
+        print(ship2.name + " destroyed")
+
 
